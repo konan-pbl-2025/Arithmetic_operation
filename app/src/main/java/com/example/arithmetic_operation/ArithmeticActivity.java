d09f1f1e28aa5bfc57fc89ca268d4ae3771d4410
AnjuTamura <ange1225ange@gmail.com>
2025-10-31 16:37:45 +0900

    ArithmeticActivityを追加しました。

---
+++ app/src/main/java/com/example/arithmetic_operation/ArithmeticActivity.java
@@ -0,0 +1,14 @@
+package com.example.arithmetic_operation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArithmeticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arithmetic);
+    }
+}
